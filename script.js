e916bde4539bc567beb3c53ebe06be47c98c5205
Andreas Ek <andreas@flowcom.se>
2012-12-30 23:05:23 +0100

    touchable

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ jQuery(document).ready(function(){
 
     /* A helper function for converting a set of elements to draggables: */
 	make_draggable(jQuery('.note'));
-    jQuery(".note").touchable();
 
     jQuery('.note').click(function(event){
 
@@ -140,6 +139,10 @@ var zIndex = 0;
 
 function make_draggable(elements)
 {
+    /* Elements is a jquery object: */
+    elements.touchable({
+        containment:'parent'
+    });
 
     /* Elements is a jquery object: */
     elements.draggable({
