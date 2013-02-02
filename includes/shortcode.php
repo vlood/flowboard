5a4d7afa662fba0fc997e3819dd1d7b65cf0ffcb
root <root@nahuel.(none)>
2013-02-02 15:22:48 +0200

    adding usernotes shortcode support

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -13,8 +13,9 @@ class FlowBoard_Shortcode {
         //This will be shown in the inline modal
         add_action( 'admin_footer', array( &$this, 'add_inline_popup_content' ) );
 
-        //Add the shortcode function
+        //Adding the shortcodes' functions
         add_shortcode( 'FlowBoard', array(&$this, 'flowboard' ));
+        add_shortcode( 'usernotes', array(&$this, 'usernotes' ));
 
     }
 
@@ -84,6 +85,51 @@ class FlowBoard_Shortcode {
         $board .= "<!--/FlowBoard-->\r\n";
 
         return $board;
+    }
+
+    function usernotes($atts){
+
+        if(!$atts) {
+            echo 'no attributes defined';
+        }
+
+        extract( $atts );
+
+        $args = array( 'post_type' => 'flowboard_note', 'posts_per_page' => 30, 'author' => $userid );
+
+        $loop = new WP_Query( $args );
+
+        if (isset($userid)) {
+            $tasks_author = get_the_author_meta('display_name', $userid);
+            echo '<h2>'.__('Responsible', 'FlowBoard').': '.$tasks_author.'</h2>';
+        }
+
+        echo '<table><tbody><tr>'.(!isset($userid) ? '<td>'.__('Responsible', 'flowboard').'</td>' : '' ).'
+                    <td>'.__('Headline', 'flowboard').'</td>
+                    <td>'.__('Text', 'flowboard').'</td>
+                    <td>'.__('Board', 'flowboard').'</td>
+                    <td>'.__('Status', 'flowboard').'</td>
+                    <td>'.__('Time left', 'flowboard').'</td>
+                    <td>'.__('Estimate', 'flowboard').'</td></tr>';
+
+        while ( $loop->have_posts() ) {
+
+            $loop->the_post();
+            $board_id = get_post_meta(get_the_ID(), '_flowboard_board');
+
+            $dataString = get_post_meta(get_the_ID(), '_flowboard_meta');
+            $note_meta = json_decode($dataString[0]);
+
+            echo '<tr>'
+                    .(!isset($userid) ? '<td>'.get_the_author_meta('display_name', $userid).'</td>' : '' ).'
+                    <td>'.get_the_title().'</td>
+                    <td>'.get_the_content().'</td>
+                    <td>'.get_post($board_id[0])->post_title.'</td>
+                    <td>'.$note_meta->status.'</td>
+                    <td>'.$note_meta->timeleft.'</td>
+                    <td>'.$note_meta->estimate.'</td></tr>';    
+        }
+        echo '</tbody></table>';
     }
 
     function media_buttons_context( $context ) {
